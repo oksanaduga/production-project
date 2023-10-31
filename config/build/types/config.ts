00a7920ebecf751bf14b5cc1ca9ba645bea9472d
oksanaduga <akakiikaramazov@gmail.com>
2023-10-31 13:10:50 +0300

    feat: add dev server and variables

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -6,8 +6,14 @@ export interface BuilPaths {
   html: string;
 }
 
+export interface BuildEnv {
+  mode: BuildMode;
+  port: number;
+}
+
 export interface BuildOptions {
   mode: BuildMode;
   paths: BuilPaths;
   isDev: boolean;
+  port: number;
 }
