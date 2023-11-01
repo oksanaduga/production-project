08ca80a6798d9268d08a3f7b97c303d1f897ff8d
oksanaduga <akakiikaramazov@gmail.com>
2023-11-01 14:38:38 +0300

    feat: add classNames

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,14 @@
+
+
+type Mods = Record<string, string | boolean>
+
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+  return [
+    cls,
+    ...additional,
+    ...Object.entries(mods)
+      .filter(([classNames, value]) => Boolean(value))
+      .map(([classNames, value]) => classNames),
+  ]
+    .join(' ');
+}
