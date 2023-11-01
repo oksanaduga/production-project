43c5714343a7c59222de325bb774771e21de942a
oksanaduga <akakiikaramazov@gmail.com>
2023-11-01 13:24:34 +0300

    feat: add toggle theme by context provider

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,6 +1,5 @@
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
-import path from "path";
 import { buildLoaders } from "./buildLoaders";
 import { buildPlugins } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
