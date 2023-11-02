8168205ba140ad9e9e05e9a92854bd9f2eb729aa
oksanaduga <akakiikaramazov@gmail.com>
2023-11-02 13:14:09 +0300

    feat: add navbar

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,10 +2,10 @@
 
 type Mods = Record<string, string | boolean>
 
-export function classNames(cls: string, mods: Mods, additional: string[]): string {
+export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
   return [
     cls,
-    ...additional,
+    ...additional.filter(Boolean),
     ...Object.entries(mods)
       .filter(([classNames, value]) => Boolean(value))
       .map(([classNames, value]) => classNames),
