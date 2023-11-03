c9b2f8f197a733f37d07676c3c3c84a7ea5e992f
oksanaduga <akakiikaramazov@gmail.com>
2023-11-03 11:44:30 +0300

    feat: add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,24 @@
-import "./styles/index.scss";
-import { useTheme } from "app/providers/ThemeProvider";
-import { classNames } from "shared/lib/classNames/classNames";
-import { AppRouter } from "./providers/router";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
+import './styles/index.scss';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   )
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from "shared/lib/classNames/classNames"
 import cls from "./Sidebar.module.scss"
 import { useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 
 
 interface SidebarProps {
@@ -20,7 +21,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
         <button onClick={onToggle}>toggle</button>
         <div className={cls.switchers}>
           <ThemeSwitcher />
-          {/* Lang switcher */}
+          <LangSwitcher className={cls.lang}/>
         </div>
       </div>
   )
