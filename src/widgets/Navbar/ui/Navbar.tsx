e20902862ada32b01ac69d456bf0b5412e70f083
oksanaduga <akakiikaramazov@gmail.com>
2023-11-15 15:34:25 +0300

    feat: add modal

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,9 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { useTranslation } from 'react-i18next';
 import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,18 +13,24 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink
-                    to={RoutePath.main}
-                    theme={AppLinkTheme.SECONDARY}
-                    className={cls.mainLink}
-                >
-                    {t('main')}
-                </AppLink>
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('login')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                gjfjgdkfjlkdfgjfjgdkfjg jgfdkjlkdfgjfjgdkfjg jggjfjgdkfjg jgfdkjlkdfgjfjgdkfjg jgfdkjlkdfgjfjgdkfjg
+            </Modal>
         </div>
     );
 };
