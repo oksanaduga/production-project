725f461cc9b8ba2d96e335f6df3c3b4e65568b37
oksanaduga <akakiikaramazov@gmail.com>
2023-11-21 09:41:06 +0300

    fix: update screenshots with i18n

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -1,31 +1,59 @@
-import i18next from 'i18next';
-import Backend from 'i18next-http-backend';
+// import i18next from 'i18next';
+// import Backend from 'i18next-http-backend';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+
+// i18next
+//     .use(Backend)
+//     .use(LanguageDetector)
+//     .use(initReactI18next)
+//     .init({
+//         fallbackLng: 'ru',
+//         debug: __IS_DEV__,
+
+//         interpolation: {
+//             escapeValue: false,
+//         },
+
+// backend: {
+//     loadPath: '/locales/{{lng}}/{{ns}}.json',
+// },
+// backend: {
+//     loadPath: () => {
+//         const isStorybook = window.location.pathname.includes('storybook-static');
+//         return isStorybook
+//             ? '/storybook-static/locales/{{lng}}/{{ns}}.json'
+//             : '/locales/{{lng}}/{{ns}}.json';
+//     },
+// },
+// });
+
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+
+import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-i18next
+// export default i18next;
+
+i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        fallbackLng: 'ru',
-        debug: __IS_DEV__,
+    // lng: 'en',
+        fallbackLng: 'en',
+        // debug: __IS_DEV__,
+        debug: true,
+        load: 'languageOnly',
 
         interpolation: {
-            escapeValue: false,
+            escapeValue: false, // not needed for react as it escapes by default
         },
-
         backend: {
             loadPath: '/locales/{{lng}}/{{ns}}.json',
         },
-        // backend: {
-        //     loadPath: () => {
-        //         const isStorybook = window.location.pathname.includes('storybook-static');
-        //         return isStorybook
-        //             ? '/storybook-static/locales/{{lng}}/{{ns}}.json'
-        //             : '/locales/{{lng}}/{{ns}}.json';
-        //     },
-        // },
+        keySeparator: false,
     });
 
-export default i18next;
+export default i18n;
