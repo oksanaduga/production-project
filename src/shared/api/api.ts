d2a1f34bb97a3739e64334abbb9e94a244d29673
oksanaduga <akakiikaramazov@gmail.com>
2023-12-06 10:26:42 +0300

    fix: api config and apiUrl path

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -5,7 +5,12 @@ import axios from 'axios';
 
 export const $api = axios.create({
     baseURL: __API__,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '',
-    },
+});
+
+$api.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.authorization = localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+    }
+
+    return config;
 });
