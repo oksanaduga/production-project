1827bb58c7393fd295ae43f8eae93c35efa57c35
oksanaduga <akakiikaramazov@gmail.com>
2023-12-26 16:21:50 +0300

    feat: isolated test

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -6,27 +6,53 @@ import { ReducersMapObject } from '@reduxjs/toolkit';
 import i18nForTest from '@/shared/config/i18n/i18nForTest';
 
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+// eslint-disable-next-line duga-plugin/layer-imports
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+import { Theme } from '@/shared/const/theme';
+// eslint-disable-next-line duga-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface ComponentRenderOptions {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;// ReducersList
+    theme?: Theme;
 }
 
-export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+export interface TestProviderProps {
+    children: ReactNode;
+    options?: ComponentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+    const { children, options = {} } = props;
+
     const {
         route = '/',
         initialState,
         asyncReducers,
+        theme = Theme.LIGHT,
     } = options;
 
-    return render(
+    return (
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
                 <I18nextProvider i18n={i18nForTest}>
-                    {component}
+                    <ThemeProvider initialTheme={theme}>
+                        <div className={`app ${theme}`}>
+                            {children}
+                        </div>
+                    </ThemeProvider>
                 </I18nextProvider>
             </StoreProvider>
-        </MemoryRouter>,
+        </MemoryRouter>
+    );
+}
+
+export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+    return render(
+        <TestProvider options={options}>
+            {component}
+        </TestProvider>,
     );
 }
